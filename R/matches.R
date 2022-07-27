3c285ccf67a56c25a788f8e24093d3dc5787a683
Natalie Weaver <natalie.weaver@milliman.com>
2022-07-26 17:26:50 -0700

    hard reset to 2.3.7, getting an error with plot snapshot tests, want to see what happens on github

--- R/matches.R
+++ R/matches.R
@@ -1,24 +1,27 @@
 #' Make a standard league-play tibble
 #'
 #' Given a league-play data frame from {engsoccer}, returns a tibble with
-#' standardized column names and types, e.g. `date` is a `Date`.
+#' standardised colomn-names and types, e.g. `date` is a `Date`.
 #'
-#' @param data_engsoc data frame obtained from {engsoccerdata}.
-#' @param country `character` scalar, specifies the league, e.g. "Spain"
+#' @param data_engsoc `data.frame` obtained from {engsoccerdata}.
+#' @param country `character` scalar, specifies the league.
+#'   `uss_countries()` returns choices available from {engsoccerdata}
 #'
-#' @return a tibble with columns `country`, `date`, `season`, `tier`, `home`,
-#'   `visitor`, `goals_home`, `goals_visitor`.
-#' @export
+#' @return [tibble][tibble::tibble-package] with columns `country`, `date`,
+#'  `season`, `tier`, `home`, `visitor`, `goals_home`, `goals_visitor`.
 #'
 #' @examples
-#' uss_make_matches(engsoccerdata::spain, "Spain")
+#' uss_make_matches(engsoccerdata::italy, "Italy")
+#' @keywords internal
+#' @export
+#'
 uss_make_matches <- function(data_engsoc, country) {
-
+  
   # validate
   validate_data_frame(data_engsoc)
   validate_cols(data_engsoc, cols_engsoc())
-
-  # put into standard form
+  
+  # put into "standard" form
   result <-
     data_engsoc |>
     tibble::as_tibble() |>
@@ -32,5 +35,6 @@ uss_make_matches <- function(data_engsoc, country) {
       goals_home = as.integer(.data$hgoal),
       goals_visitor = as.integer(.data$vgoal)
     )
+  
   result
 }
